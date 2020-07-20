f41b2513bc86c58759b6bd0e537fd67fbc4a3621
randomtree07 <ryanfeldbush@gmail.com>
2020-07-20 15:52:02 -0400

    Changed build destination

--- build-geos.sh
+++ build-geos.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
+mkdir build
 cd geos/build
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/usr/local ..
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/build ..
 make -j 8 VERBOSE=1 install
 make clean
